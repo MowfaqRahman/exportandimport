b2748dbfb94c3fefc6995246587351ed69b8c6d8
MowfaqRahman <mowfaqrahman01@gmail.com>
2025-12-07 20:18:19 +0530

    Fix deploying bug

--- src/dashboard/page.tsx
+++ src/dashboard/page.tsx
@@ -2,6 +2,7 @@ import DashboardNavbar from "@/components/dashboard-navbar";
 import { redirect } from "next/navigation";
 import { createClient } from "../../supabase/server";
 import DashboardContent from "@/components/dashboard/dashboard-content";
+import { Purchase } from "@/types/business";
 
 export default async function Dashboard() {
   const supabase = await createClient();
@@ -19,20 +20,9 @@ export default async function Dashboard() {
   const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
   const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
 
-  const [salesData, expensesData] = await Promise.all([
-    supabase
-      .from('sales')
-      .select('*')
-      .gte('date', firstDayOfMonth.toISOString().split('T')[0])
-      .lte('date', lastDayOfMonth.toISOString().split('T')[0])
-      .order('date', { ascending: true }),
-    supabase
-      .from('expenses')
-      .select('*')
-      .gte('date', firstDayOfMonth.toISOString().split('T')[0])
-      .lte('date', lastDayOfMonth.toISOString().split('T')[0])
-      .order('date', { ascending: true }),
-  ]);
+  const { data: salesData } = await supabase.from("sales").select("*").order("date", { ascending: false });
+  const { data: expensesData } = await supabase.from("expenses").select("*").order("date", { ascending: false });
+  const { data: purchasesData } = await supabase.from("purchase").select("*").order("date", { ascending: false });
 
   return (
     <>
@@ -41,6 +31,7 @@ export default async function Dashboard() {
         <DashboardContent 
           initialSales={salesData.data || []}
           initialExpenses={expensesData.data || []}
+          initialPurchases={purchasesData.data || []}
         />
       </main>
     </>
